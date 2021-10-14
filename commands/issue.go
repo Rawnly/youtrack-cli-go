06a13a1ae47cff22f88200cdfb4bd1f89df3a037
Rawnly <fedevitale99@gmail.com>
2021-10-14 15:04:23 +0200

    feat: issue-time command

--- commands/issue.go
+++ commands/issue.go
@@ -10,11 +10,24 @@ import (
 )
 
 func GetIssueCmd(storage *util.Storage) *cobra.Command  {
-	return &cobra.Command{
-		Use: "issue <issueId>",
-		Short: "Get issue info",
+  cmd := &cobra.Command{
+		Use: "issue [commands]",
+		Short: "Issue Operations",
+	}
+
+  cmd.AddCommand(IssueInfoCmd(storage))
+  cmd.AddCommand(IssueTimeCmd(storage))
+
+  return cmd
+}
+
+func IssueInfoCmd(storage *util.Storage) *cobra.Command {
+  cmd := &cobra.Command{
+    Use: "get <issue>",
+    Short: "Get issue infos",
+    Long: "Display issue infos",
 		Args: cobra.ExactValidArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
+    Run: func(cmd *cobra.Command, args []string) {
 			issue, err := api.FetchIssue(args[0], storage)
 
 			if err != nil {
@@ -40,8 +53,36 @@ func GetIssueCmd(storage *util.Storage) *cobra.Command  {
 				cmd.PrintErr(err.Error())
 				return
 			}
-		},
-	}
+    },
+  }
+
+  return cmd
+}
+
+func IssueTimeCmd(storage *util.Storage) *cobra.Command {
+  cmd := &cobra.Command{
+    Use: "time <issue>",
+    Short: "Display spentTime on an issue",
+    Run: func(cmd *cobra.Command, args []string) {
+      issueId := args[0]
+      
+      issue, err := api.FetchIssue(issueId, storage)
+
+      if err != nil {
+        cmd.PrintErr(err.Error())
+      }
+
+      estimation, err := api.GetPeriodicIssueCustomField(*issue, "Estimation", *storage)
+
+      if err != nil {
+        cmd.PrintErr(err.Error())
+      }
+
+      cmd.Println(estimation.Value.Presentation)
+    },
+  }
+
+  return cmd
 }
 
 
@@ -69,3 +110,6 @@ func PrintIssue(w io.Writer, issue api.Issue, estimation api.PeriodIssueCustomFi
 	`),
 	}.Execute(w)
 }
+
+
+
